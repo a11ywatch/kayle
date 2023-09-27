32013299750b066ffcb13441ebc73cac544500da
j-mendez <jeff@a11ywatch.com>
2023-09-27 10:08:17 -0400

    test(full): add all runners

---
+++ kayle/tests/full.ts
@@ -0,0 +1,40 @@
+// run fast_axe, fast_htmlcs, and ace
+import assert from "assert";
+import puppeteer from "puppeteer";
+import { Standard, kayle } from "kayle";
+import { drakeMock } from "./mocks/html-mock";
+import { performance } from "perf_hooks";
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  if (process.env.LOG_ENABLED) {
+    page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+  }
+  const startTime = performance.now();
+  const { issues, pageUrl, documentTitle, meta, automateable } = await kayle({
+    page,
+    browser,
+    runners: ["htmlcs", "axe", "ace"],
+    includeWarnings: true,
+    html: drakeMock,
+    standard: Standard.WCAG2AA,
+    origin: "https://www.drake.com", // origin is the fake url in place of the raw content
+  });
+  const nextTime = performance.now() - startTime;
+
+  console.log(meta);
+  console.log(automateable);
+  console.log("time took", nextTime);
+
+  // valid list
+  assert(Array.isArray(issues));
+  assert(meta.errorCount === 88);
+  assert(meta.warningCount === 51);
+  assert(meta.accessScore === 30);
+
+  assert(typeof pageUrl === "string");
+  assert(typeof documentTitle === "string");
+
+  await browser.close();
+})();
