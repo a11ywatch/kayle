f6369af213299cb0ac40a5946db137aa31c0f0b7
j-mendez <jeff@a11ywatch.com>
2024-03-01 11:25:57 -0500

    test(kayle): add split wasm kayle runner

--- kayle/tests/basic-kayle.ts
+++ kayle/tests/basic-kayle.ts
@@ -14,26 +14,24 @@ import { performance } from "perf_hooks";
   const { issues, pageUrl, documentTitle, meta, automateable } = await kayle({
     page,
     browser,
-    runners: ["htmlcs", "axe"],
+    runners: ["kayle"],
     includeWarnings: true,
     html: drakeMock,
     standard: Standard.WCAG2AA,
-    origin: "https://www.drake.com", // origin is the fake url in place of the raw content
+    origin: "https://www.drake.com",
   });
   const nextTime = performance.now() - startTime;
 
+  console.log(`Issue count ${issues.length}`);
   console.log(meta);
   console.log(automateable);
   console.log("time took", nextTime);
 
   // valid list
   assert(Array.isArray(issues));
-  assert(meta.errorCount <= 45);
-  assert(meta.warningCount === 43);
-  assert(meta.accessScore <= 70);
-
   assert(typeof pageUrl === "string");
   assert(typeof documentTitle === "string");
+  assert(meta.errorCount <= 4);
 
   await browser.close();
 })();
